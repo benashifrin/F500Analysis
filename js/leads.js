32ebb83316f9b74301bc19d5a67531fd7d10be68
Ben Shifrin <benashifrin@gmail.com>
2023-08-17 16:26:53 -0500

    init

---
+++ js/leads.js
@@ -0,0 +1,9 @@
+// Some DataSets are massive and will bring any web browser to its knees if you
+// try to load the entire thing. To keep your app performing optimally, take
+// advantage of filtering, aggregations, and group by's to bring down just the
+// data your app needs. Do not include all columns in your data mapping file,
+// just the ones you need.
+//
+// For additional documentation on how you can query your data, please refer to
+// https://developer.domo.com/docs/dev-studio/dev-studio-data
+domo.get("/data/v1/leads").then((data) => drawTable(data, "leads-table"));
